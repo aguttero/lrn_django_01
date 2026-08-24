b34ca76cffa843d2d31aa2f728c6163bbaee7659
alejandro guttero <aguttero@gmail.com>
2026-08-24 18:43:41 -0400

    0824 s44 base.html - extends

--- src/challenges/urls.py
+++ src/challenges/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 # name attribute used fpr reverse URL -> with redirect(name,string)
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index_old, name="index"),
     path("pol", views.pol, name="pol"),
     path("healthy", views.healthy, name="healthy"),
     path("<int:month>", views.monthly_challenge_by_number, name="monthly_challenge_number"),
-    path("<str:month>", views.monthly_challenge, name="month_challenge"),
+    path("<str:month>", views.monthly_challenge, name="month_str_path"),
 
 ]
--- src/challenges/views.py
+++ src/challenges/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 monthly_content_dict = {
     "jan": "Eat no meat",
@@ -27,11 +27,13 @@ def healthy(request):
     return HttpResponse("<h1>Hello. This is the January Month page</h1>")
 
 
-def index(request):
-    response_text = b"Place holder for index"
-    list_items = []
-    months_list = list(monthly_content_dict.keys())
-    return HttpResponse(response_text)
+# WITH TEMPLATE RENDER
+def index_old(request):
+    month_list = list(monthly_content_dict.keys())
+    context_dict = {
+        "month_list": month_list,
+    }
+    return render(request, "challenges/index.html", context_dict)
 
 
 # filter by int and redirect
@@ -39,8 +41,8 @@ def index(request):
 def monthly_challenge_by_number(request, month: int):
     months_list1 = list(monthly_content_dict.keys())
 
-    print (f"month value={month} type={type(month)}")
-    print (f"months_list1= {months_list1}")
+    print(f"month value={month} type={type(month)}")
+    print(f"months_list1= {months_list1}")
 
     # VALIDATE input is whitin index list
     if month > len(months_list1):
@@ -51,22 +53,11 @@ def monthly_challenge_by_number(request, month: int):
     # return redirect("/challenges/"+redirect_month)
 
     # REVERSE REDIRECT (NAME in urls.py name attribute)
-    redirect_month = months_list1[month-1]
+    redirect_month = months_list1[month - 1]
     # OLD WAY using reverse
     # from django.ulrs import reverse
-    #redirect_path = reverse("month-challenge", args=[redirect_month])
-    return redirect ("month_challenge", redirect_month )
-
-# V2 WITH RENDER
-def monthly_challenge(request, month: str):
-    challenge_text = monthly_content_dict.get(month,None)
-    print (f"challenge_text= {challenge_text}")
-    if month == "badurl":
-        raise Http404("sample 404 response")
-    if challenge_text:
-        return render(request=request, template_name="challenges/challenge_item.html")
-    else:
-        raise Http404("ZAG 404: Month not found")
+    # redirect_path = reverse("month-challenge", args=[redirect_month])
+    return redirect("month_str_path", redirect_month)
 
 
 # V1 without RENDER
@@ -79,3 +70,20 @@ def monthly_challenge(request, month: str):
 #         return HttpResponse(f"Challenge for {month}: {challenge_text}")
 #     else:
 #         raise Http404("ZAG 404: Month not found")
+
+
+# V2 WITH RENDER
+def monthly_challenge(request, month: str):
+    challenge_text = monthly_content_dict.get(month, None)
+    print(f"challenge_text= {challenge_text}")
+    if month == "badurl":
+        raise Http404("sample 404 response")
+    if challenge_text or not challenge_text: # para que aparezca el None en el e render del template
+        context_dict = {"challenge_item": challenge_text, "selected_month": month}
+        return render(
+            request=request,
+            template_name="challenges/challenge_item.html",
+            context=context_dict,
+        )
+    else:
+        raise Http404("ZAG 404: Month not found")
